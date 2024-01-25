2145b910af1a8b59d097025d71294c091b3d4bf5
Nugget-Meister <tremainecamacho@gmail.com>
2024-01-24 21:42:59 -0500

    barebones for back end

---
+++ app.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Controller Import
+
+
+//
+app.use(cors())
+app.use(express.json())
+
+// Controller use
+
+
+// Base Routes
+app.get('/', (req, res) => {
+    res.send("Hi.")
+})
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+        message: "No such route exists",
+        data: null
+    })
+})
