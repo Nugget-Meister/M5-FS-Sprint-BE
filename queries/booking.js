122ec7537aa5a008423d21bc729578945d945795
Nugget-Meister <tremainecamacho@gmail.com>
2024-01-26 18:27:35 -0500

    boilerplate for bookings queries

---
+++ queries/booking.js
@@ -0,0 +1,15 @@
+const db =  require("../db/dbConfig.js");
+
+const getAllBookings = () => {}
+const getSingleBooking = (id) => {}
+const createBooking = (data) => {}
+const updateBooking = (id, data) => {}
+const deleteBooking = () => {}
+
+module.exports = {
+    getAllBookings,
+    getSingleBooking,
+    createBooking,
+    updateBooking,
+    deleteBooking
+}
